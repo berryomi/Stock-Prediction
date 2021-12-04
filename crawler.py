aa046892196eb3f76eb03c9281d2ee8c995e6f91
wnwkr13 <wnwkr13@gmail.com>
2021-12-04 17:24:45 +0900

    add crawler

--- crawler.py
+++ crawler.py
@@ -1,71 +1,111 @@
 import pandas as pd
 import numpy as np
 import os
-import requests as rq
-import pandas_datareader as web
+
 from datetime import date
-import time
-from tqdm import tqdm
+
+import FinanceDataReader as fdr
+
+''''
+InstructionsL:
+-----------Install 
+ - pip install -U finance-datareader
+
 
 '''
-description : Get ticker from NASDAQ and then ticker cleansing (Get rid of SPAC, Preferred Shares, ETF, Fund) 
-then Sort ticker by ascending order 
-Parameter: N/A
-Return value: N/A
-'''
-def get_ticker():
-    url = 'https://api.nasdaq.com/api/screener/stocks?tableonly=true&limit=25&offset=0&download=true'
-    headers = {"User-Agent": "Mozilla/5.0 (X11; CrOS x86_64 12871.102.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.141 Safari/537.36"}
 
-    data = rq.get(url, headers=headers)
-    data_json = data.json()['data']
-    all_data = pd.DataFrame(data = data_json['rows'], columns = data_json['headers'])
-    all_data = all_data.loc[~all_data.symbol.duplicated(keep = 'first'), :]
+def get_all_ticker(symbol):
+    if symbol == 1:
+        df_NYSDAQ = fdr.StockListing("NASDAQ")
+        return df_NYSDAQ
+    elif symbol == 2:
+        df_NYSE = fdr.StockListing("NYSE")
+        return df_NYSE
+    elif symbol == 3:
+        df_AMEX = fdr.StockListing("AMEX")
+        return df_AMEX
+    elif symbol == 4:
+        df_SP500 = fdr.StockListing("SP500")
+        return df_SP500
 
 
-    all_data['marketCap'] = pd.to_numeric(all_data['marketCap']) 
-    all_data = all_data.loc[all_data['marketCap'] != 0]
-    all_data = all_data.loc[~all_data['symbol'].str.contains('\\^')]
-    all_data = all_data.loc[~all_data['name'].str.contains('Preferred')]
-    all_data = all_data.loc[~all_data['name'].str.contains('preferred')]
-    all_data = all_data.loc[~all_data['name'].str.contains('ETF')]
-    all_data = all_data.loc[~all_data['name'].str.contains('Fund')]
-    all_data = all_data.loc[~all_data['name'].str.contains('%')]
-    all_data['symbol'] = all_data['symbol'].str.replace('\\/', '-')
-    all_data = all_data.sort_values(by = 'symbol')
-    all_data = all_data.reset_index(drop=True)
 
-    all_data.to_csv("data/US_ticker.csv")
-    print("Complete for getting ticker from NASDAQ and save it to CSV")
+def get_stock_price(ticker, start_date=None, end_date=None):
+    stock = fdr.DataReader(ticker, start_date, end_date)
+    df_stock_price = pd.DataFrame(stock)
+    df_stock_price = df_stock_price.drop('Change', axis=1)
+    return df_stock_price
 
-'''
-description : Get stock data 
-Parameter: N/A
-Return value: N/A
-'''
-
-def get_Stock_data():
-    US_ticker = pd.read_csv('data/US_ticker.csv', index_col=0)
-    error_list = []
+def main():
+    ans_type = int(input("All stock data from specific market => Press 1 , specific stock data => Press 2, Indices => Press 3:     "))
     
     if not os.path.exists("data/US_price"):
         os.makedirs("data/US_price")
     
-    for ticker in tqdm(range(0, len(US_ticker))):
-        
-        price = pd.DataFrame({"Price" : [np.nan]})
-        price.index = [date.today().strftime("%Y-%m-%d")]
-        
-        name = US_ticker["symbol"][ticker]
-        
+    elif not os.path.exists("data/Indices"):
+        os.makedirs("data/Indices")
+            
+            
+    if ans_type == 1:
         try:
-            price = web.DataReader(name, "yahoo")
+            market_Type = int(input("NASDAQ = 1, NYSE = 2, AMEX = 3, S&P500 = 4:    "))
+            all_tickers = get_all_ticker(market_Type)
+            ticker_list= []
+                
+            for ticker in all_tickers["Symbol"]:
+                ticker_list.append(ticker)
+            
+            start_date = input("Type the start date, if you don't want to start with the specific date, JUST Press Enter  ")
+            end_date = input("Type the end date, if you don't want to start with the specific date, JUST Press Enter ")
+                
+            for stock in ticker_list:
+                if (start_date or end_date) == "":
+                    df = get_stock_price(stock)
+                    df.to_csv('data/US_price/'+ stock +'_price.csv')
+                else:
+                    df = get_stock_price(stock, start_date, end_date)
+                    df.to_csv('data/US_price/'+ ticker +'_price.csv')
+            print("Done")
+                
         except Exception as e:
-            print(e)
-            error_list.append(name)
+            print("Error is " + str(e))
         
-        price.to_csv('data/US_price/'+name+'_price.csv')
-        time.sleep(2)  
+            
+        
+    elif ans_type == 2:
+        ticker = input("Type the ticker that you want to get the stock price(Ex apple = AAPL, AMAZON = AMZN....)     ")
+        start_date = input("Type the start date, if you don't want to start with the specific date, JUST Press Enter  ")
+        end_date = input("Type the end date, if you don't want to start with the specific date, JUST Press Enter ")
+        
+        
+
+        try: 
+            if (start_date or end_date) == "":
+                df = get_stock_price(ticker)
+                df.to_csv('data/US_price/'+ ticker +'_price.csv')
+            else:
+                df = get_stock_price(ticker, start_date, end_date)
+                df.to_csv('data/US_price/'+ ticker +'_price.csv')
+            print("Done")
+        except Exception as e:
+            print("Error is " + str(e))
+            
+    elif ans_type == 3:
+        indices_Type = input("Indices[Dow] = DJI, Indices[Nasdaq] = IXIC, Indices[US500] = 7: US500  ")
+        start_date = input("Type the start date, if you don't want to start with the specific date, JUST Press Enter  ")
+        end_date = input("Type the end date, if you don't want to start with the specific date, JUST Press Enter ")
+        if (start_date or end_date) == "":
+            df = get_stock_price(indices_Type)
+            df.to_csv('data/Indices/'+ indices_Type +'_price.csv')
+        else:
+            df = get_stock_price(indices_Type, start_date, end_date)
+            df.to_csv('data/Indices/'+ indices_Type +'_price.csv')
+        print("done")
+    else:
+        print("Press either 1 or 2 or 3")
+        
+
+
         
 if __name__ == '__main__':
-    get_Stock_data()
+    main()
